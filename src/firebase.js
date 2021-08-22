44afd47fb3d33b480af72f039f04c45f95c72329
ismail449 <44037598+ismail449@users.noreply.github.com>
2021-08-22 21:16:57 +0200

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -1,13 +1,13 @@
 import firebase from 'firebase'
 
 const firebaseConfig = {
-  apiKey: 'AIzaSyBBVPep2KjtM6a2yinnH6tYmyoGpo3iohs',
-  authDomain: 'instagram-clone-fec09.firebaseapp.com',
-  projectId: 'instagram-clone-fec09',
-  storageBucket: 'instagram-clone-fec09.appspot.com',
-  messagingSenderId: '832751791892',
-  appId: '1:832751791892:web:ea8c97fa893da4a0e79c01',
-  measurementId: 'G-QQTPMT3VBH',
+  apiKey: '',
+  authDomain: '',
+  projectId: '',
+  storageBucket: '',
+  messagingSenderId: '',
+  appId: '',
+  measurementId: '',
 };
 
 const firebaseApp = firebase.initializeApp(firebaseConfig);
